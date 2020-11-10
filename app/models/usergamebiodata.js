8d5769c4f0bf0819a3072d0ca727350d1745bcfb
Muh Naofal Mufid <valnovc@gmail.com>
2020-11-10 21:37:44 +0700

    try add passport-local but failed

--- app/models/usergamebiodata.js
+++ app/models/usergamebiodata.js
@@ -1,51 +1,51 @@
 module.exports = (sequelize, Sequelize) => {
-  const UserGameBiodata = sequelize.define("user_game_biodata", {
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER
-    },
-    user_id:{
-      type: Sequelize.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'user_games',
-        key: 'user_id'
-      } 
-    },
-    firstname: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    lastname: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    gender: {
-      type: Sequelize.STRING
-    },
-    address: {
-      type: Sequelize.TEXT
-    },
-    avatar: {
-      type: Sequelize.STRING
-    },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE
+    const UserGameBiodata = sequelize.define("user_game_biodata", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'user_games',
+          key: 'user_id'
+        } 
+      },
+      firstname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.TEXT
+      },
+      avatar: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+    UserGameBiodata.associate = function (models) {
+      UserGameBiodata.belongsTo(models.UserGames, {})
     }
-  })
-  UserGameBiodata.associate = function (models) {
-    UserGameBiodata.belongsTo(models.UserGames, {})
+    return UserGameBiodata
   }
-  return UserGameBiodata
-}
--- app/models/usergamehistory.js
+++ app/models/usergamehistory.js
@@ -1,37 +1,37 @@
 module.exports = (sequelize, Sequelize) => {
-  const UserGameHistory = sequelize.define("user_game_history", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      user_id:{
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: {
-          model: 'user_games',
-          key: 'user_id'
-        } 
-      },
-      score: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-      },
-      lastPlay: {
-        type: Sequelize.DATE
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
+    const UserGameHistory = sequelize.define("user_game_history", {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        user_id:{
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'user_games',
+            key: 'user_id'
+          } 
+        },
+        score: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        lastPlay: {
+          type: Sequelize.DATE
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      })
+      UserGameHistory.associate = function (models) {
+        UserGameHistory.belongsTo(models.UserGames, {})
       }
-    })
-    UserGameHistory.associate = function (models) {
-      UserGameHistory.belongsTo(models.UserGames, {})
-    }
-    return UserGameHistory
-}
+      return UserGameHistory
+  }
