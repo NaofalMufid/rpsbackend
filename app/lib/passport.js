3335c65f059144911063c123637530cf938a4b70
Muh Naofal Mufid <valnovc@gmail.com>
2020-12-09 16:47:45 +0700

    update configdb

--- app/lib/passport.js
+++ app/lib/passport.js
@@ -7,10 +7,10 @@ const options = {
     jwtFromRequest : ExtractJwt .fromHeader ('authorization' ),
     secretOrKey : 'cukupkitayangtahu' ,
 }
-passport .use(new JwtStrategy (options, async (payload, done) => {
+passport.use(new JwtStrategy (options, async (payload, done) => {
     User.findByPk (payload .id)
     .then(user => done(null, user))
     .catch(err => done(err, false))
 }))
 
-module .exports = passport
+module.exports = passport
--- app/models/userhistory.js
+++ app/models/userhistory.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'UserHistory',
   });
-  UserHistory.associat = function (models) {
+  UserHistory.associate = function (models) {
     UserHistory.belongsTo(models.User, {})
   }
   return UserHistory;
